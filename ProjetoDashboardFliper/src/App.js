9f849652d8cc4fbd37bed546fae182cdc75ec6de
Higor Martinez <68279411+HigorMartinez@users.noreply.github.com>
2021-07-12 11:22:17 -0300

    added code

---
+++ ProjetoDashboardFliper/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, Fragment } from 'react';
+import { useQuery } from "@apollo/client";
+import { useDispatch } from 'react-redux';
+
+import { GET_WEALTH_SUMMARY_DATA } from './services/Queries';
+import { setData, setLoading, setError } from './store/dashboard-actions';
+import Dashboard from './pages/Dashboard';
+
+
+const App = () => {
+
+  const dispatch = useDispatch();
+
+  const { data, loading, error } = useQuery(GET_WEALTH_SUMMARY_DATA);
+
+  console.log(data);
+
+  useEffect(() => {
+    
+
+    if (data) {
+      dispatch(setData(data.wealthSummary));
+      dispatch(setLoading(loading));
+    }
+
+    if (error) {
+      dispatch(setError(error.message));
+      dispatch(setLoading(loading));
+    }
+   
+  }, [dispatch, data, loading, error]); 
+  
+  
+  return (
+    <Fragment>
+      <Dashboard/>
+    </Fragment>
+  )
+}
+
+export default App;
