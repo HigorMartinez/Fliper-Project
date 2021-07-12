c5f215334f460217d279e15eddb553e9ed0003e6
Higor Martinez <68279411+HigorMartinez@users.noreply.github.com>
2021-07-12 11:33:34 -0300

    added code

---
+++ ProjetoDashboardFliper/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import classes from './Layout.module.css';
+
+
+const Layout = (props) => {
+  return (
+    <div className={classes.layout}>
+      <main>{props.children}</main>
+    </div>
+  );
+};
+
+export default Layout;
